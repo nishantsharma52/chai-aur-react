a78e6d74473518570e01c43c55b74d351e3b0391
Nishant Sharma <nishnatsharma5330@gmail.com>
2026-03-13 18:09:16 +0530

    Initialize React app with Vite and basic components

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,14 @@
+
+import Chai from "./Chai";
+
+function App() {
+  return (
+    <>
+    <Chai/>
+    <h1>Chai Or Code</h1>
+    </>
+
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./Chai"
+
+function App() {
+
+  return (
+    <>
+    <h1> LEARN CHAI OR REACT </h1>
+    <Chai/>
+    
+    </>
+  )
+}
+
+export default App
