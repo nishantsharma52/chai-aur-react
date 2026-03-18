a30ce039071798924a355fef11e9c185d004cce8
Nishant Sharma <nishantsharma5330@gmail.com>
2026-03-18 12:27:50 +0530

    Add Redux Toolkit and Todo components

--- reduxToolkitTodo/vite.config.js
+++ reduxToolkitTodo/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // Ye line check karein
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // Ye plugin yahan hona chahiye
+  ],
 })
